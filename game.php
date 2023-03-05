caf750b69bdbab9fa19e123e793a19c64555882a
7 <hisuinohoshi@gmail.com>
2023-03-06 03:53:20 +0800

    fix
    修复：
    通过 >>进入游戏 加载界面时，消除了时效性技能却没有保存玩家数据的问题；

--- game.php
+++ game.php
@@ -133,7 +133,18 @@ elseif((strpos($action,'neut')===0)){
 }
 if($hp > 0 && $coldtimeon && $showcoldtimer && $rmcdtime){$log .= "行动冷却时间：<span id=\"timer\" class=\"yellow\">0.0</span>秒<script type=\"text/javascript\">demiSecTimerStarter($rmcdtime);</script><br>";}
 //如果身上存在时效性技能，检查技能是否超时
-if($hp > 0 && !empty($clbpara['lasttimes'])) check_skilllasttimes();
+if($hp > 0 && !empty($clbpara['lasttimes']))
+{
+	$flag = check_skilllasttimes();
+	if($flag)
+	{
+		$pdata = current_player_save();
+		$pdata['clbpara'] = get_clbpara($pdata['clbpara']);
+		extract($pdata);
+		init_playerdata();
+		init_profile();
+	}
+}
 if($hp > 0 && in_array('inf_dizzy',$clbpara['skill']))
 {
 	$dizzy_times = (($clbpara['starttimes']['inf_dizzy'] + $clbpara['lasttimes']['inf_dizzy']) - $now)*1000;
