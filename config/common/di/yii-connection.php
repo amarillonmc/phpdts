39c4717eba5adccf3c574e6e062155e53b130cc0
nahakyuu <root@aroa.me>
2023-04-05 07:51:48 +0800

    update config

--- config/common/di/yii-connection.php
+++ config/common/di/yii-connection.php
@@ -2,20 +2,36 @@
 
 declare(strict_types=1);
 
+use yiisoft\Definitions\DynamicReference;
+use yiisoft\Definitions\Reference;
+
 use Yiisoft\Db\Connection\ConnectionInterface;
 use Yiisoft\Db\Mysql\ConnectionPDO;
 
 use Yiisoft\Db\Driver\PDO\PDODriverInterface;
 use Yiisoft\Db\Mysql\PDODriver;
 
+use Yiisoft\Db\Mysql\Dsn;
+
+/** @var array $params */
+
 return [
     ConnectionInterface::class => ConnectionPDO::class,
     PDODriverInterface::class => PDODriver::class,
     PDODriver::class => [
         '__construct()' => [
-            'dsn' => $params['db']['dsn'],
+            'dsn' => DynamicReference::to(static fn (Dsn $dsn) => $dsn->asString()),
             'username' => $params['db']['username'],
             'password' => $params['db']['password'],
+        ]
+    ],
+    Dsn::class => [
+        '__construct()' => [
+            'driver' => $params['db']['driver'],
+            'host' => $params['db']['host'],
+            'databaseName' => $params['db']['databaseName'],
+            'port' => $params['db']['port'],
+            'options' => $params['db']['options']
         ]
     ]
 ];
--- config/web/params.php
+++ config/web/params.php
@@ -2,5 +2,15 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Definitions\Reference;
+
 return [
+    'yiisoft/aliases' => [
+        'aliases' => [
+            '@public' => '@root/public',
+            '@assets' => '@public/assets',
+            '@layout' => '@root/templates/default',
+            '@views' => '@root/templates/default',
+        ],
+    ],
 ];
